3bffa04daadb904f3b4266b5ccb8062409e5bfe5
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-05-06 18:11:43 +0700

    style user interface

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import getWeather from "./fetching.js";
-const weatherForm = document.querySelector("#weather-searching-form");
+import renderUI from "./displayController.js";
 
-weatherForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const location = document.querySelector("#weather-searchbar").value;
-
-  try {
-    const data = await getWeather(location);
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-});
+renderUI();
